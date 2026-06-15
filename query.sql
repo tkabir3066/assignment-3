c01ada7dff3046607f043b3e9f5988acc5a210e3
Tutul <tutulkabir@gmail.com>
2026-06-15 16:52:00 +0530

    modified in tables data type

--- query.sql
+++ query.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS Users;
 -- 1. CREATE USERS TABLE
 -- =========================================================================
 CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (
@@ -32,7 +32,7 @@ CREATE TABLE Users (
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
 CREATE TABLE Matches (
-    match_id INT PRIMARY KEY,
+    match_id SERIAL PRIMARY KEY,
     fixture VARCHAR(255) NOT NULL,
     tournament_category VARCHAR(100) NOT NULL,
     base_ticket_price DECIMAL(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
@@ -50,7 +50,7 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id INT PRIMARY KEY,
+    booking_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     match_id INT NOT NULL,
     seat_number VARCHAR(20),
